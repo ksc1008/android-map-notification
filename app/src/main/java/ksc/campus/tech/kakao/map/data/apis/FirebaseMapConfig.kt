5048c986ffa3a890cc0b5ba4c1b42ea1715235ca
ksc1008 <ksc1008@naver.com>
2024-08-02 14:20:58 +0900

    feat: now get serviceMessage from remote config

--- app/src/main/java/ksc/campus/tech/kakao/map/data/apis/FirebaseMapConfig.kt
+++
@@ -1,9 +0,0 @@
-package ksc.campus.tech.kakao.map.data.apis
-
-import com.google.firebase.remoteconfig.FirebaseRemoteConfig
-import kotlinx.coroutines.tasks.await
-import javax.inject.Inject
-
-class FirebaseMapConfig @Inject constructor(private val remoteConfig: FirebaseRemoteConfig) {
-
-}
